1de902bad61a31e87c4d1e8bb7d91bf3e473e375
Ícaro Pavan <icaro.pavan97@gmail.com>
2025-02-28 12:11:50 -0300

    updated exercise 06

--- college/006. Traduzindo um diagrama de classes para algoritmo Pt. II/Main.java
+++ college/006. Traduzindo um diagrama de classes para algoritmo Pt. II/Main.java
@@ -4,12 +4,12 @@ public class Main {
         Catalogo catalogo = new Catalogo();
         
         // instanciando veículos de diferentes subclasses
-        Veiculo carro1 = new Carro(1, new Modelo(1, "Fiesta"), new Marca(1, "Ford"), 2008, 4, 5, 115f);
-        Veiculo carro2 = new Carro(2, new Modelo(2, "Civic"), new Marca(2, "Honda"), 2015, 4, 5, 155f);
-        Veiculo carro3 = new Carro(3, new Modelo(3, "Agile"), new Marca(3, "Chevrolet"), 2012, 4, 5, 102f);
-        Veiculo caminhao1 = new Caminhao(4, new Modelo(4, "FH"), new Marca(4, "Volvo"), 2022, 3);
-        Veiculo caminhao2 = new Caminhao(5, new Modelo(5, "Actros"), new Marca(5, "Mercedes-Benz"), 2019, 3);
-        Veiculo onibus1 = new Onibus(6, new Modelo(6, "Citywide"), new Marca(6, "Scania"), 2021, 50, true);
+        Carro carro1 = new Carro(1, new Modelo(1, "Fiesta"), new Marca(1, "Ford"), 2008, 4, 5, 115f);
+        Carro carro2 = new Carro(2, new Modelo(2, "Civic"), new Marca(2, "Honda"), 2015, 4, 5, 155f);
+        Carro carro3 = new Carro(3, new Modelo(3, "Agile"), new Marca(3, "Chevrolet"), 2012, 4, 5, 102f);
+        Caminhao caminhao1 = new Caminhao(4, new Modelo(4, "FH"), new Marca(4, "Volvo"), 2022, 3);
+        Caminhao caminhao2 = new Caminhao(5, new Modelo(5, "Actros"), new Marca(5, "Mercedes-Benz"), 2019, 3);
+        Onibus onibus1 = new Onibus(6, new Modelo(6, "Citywide"), new Marca(6, "Scania"), 2021, 50, true);
         
         // adicionando veículos ao catálogo
         catalogo.addVeiculo(carro1);
